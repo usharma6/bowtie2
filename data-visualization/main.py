48e332433143d912615bc853884633b8b304948a
Udit Sharma <usharma6@jhu.edu>
2019-05-18 16:38:50 -0400

    created a main function

---
+++ data-visualization/main.py
@@ -0,0 +1,32 @@
+from parser import parse
+from make_graphs import *
+import sys
+import plotly
+import plotly.graph_objs as go
+
+readNumber = sys.argv[1]
+readSize = sys.argv[2]
+filename = 'eg' + readNumber + '.sam'
+
+forward_reads, reverse_reads, unmatched_reads, read_quality, match_scores = parse(filename, readSize)
+labels, values = matched_vs_unmatched_pie_chart(forward_reads, reverse_reads, unmatched_reads)
+read_quality_converter(read_quality)
+data = read_quality_box_plot(read_quality)
+
+plotly.offline.plot({
+    "data" : [go.Pie(labels = labels, values = values)],
+    "layout": go.Layout(title = "Matched vs. Unmatched")
+})
+
+plotly.offline.plot({
+    "data" : [go.Histogram(x = match_scores, name = "Match Score", xbins=dict(
+        start=-12,
+        end=0))],
+    "layout": go.Layout(title = "Match Scores")
+})
+
+layout = go.Layout(
+    title = "Read Quality Scores by Location (Percent Change of Accuracy)"
+)
+fig = go.Figure(data=data, layout=layout)
+plotly.offline.plot(fig)
--- data-visualization/make_graphs.py
+++ data-visualization/make_graphs.py
@@ -2,9 +2,8 @@ from parser import parse
 import plotly.plotly as py
 import plotly
 import plotly.graph_objs as go
-forward_reads, reverse_reads, unmatched_reads, read_quality, match_scores = parse()
 
-def matched_vs_unmatched_pie_chart():
+def matched_vs_unmatched_pie_chart(forward_reads, reverse_reads, unmatched_reads):
     match_chart_labels = ['Forward Reads(Matched)', 'Reverse Reads (Matched)', 'Unmatched Reads']
     match_chart_values = [forward_reads, reverse_reads, unmatched_reads]
     return (match_chart_labels, match_chart_values)
@@ -17,13 +16,13 @@ def q_to_p(Q):
   #Turn Phred-scaled integer into error probability
   return 10.0 ** (-0.1 * Q)
 
-def read_quality_converter():
+def read_quality_converter(read_quality):
     for i in range(len(read_quality)):
         for j in range(len(read_quality[i])):
             #Converting the read quality data to probabilities
             read_quality[i][j] = 100 - int(100 * q_to_p(phred33_to_q(read_quality[i][j])))
 
-def read_quality_box_plot():
+def read_quality_box_plot(read_quality):
     data = []
     for i in range(10):
         temp = []
@@ -39,24 +38,3 @@ def read_quality_box_plot():
         )
         data.append(trace)
     return data
-
-read_quality_converter()
-labels, values = matched_vs_unmatched_pie_chart()
-plotly.offline.plot({
-    "data" : [go.Pie(labels = labels, values = values)],
-    "layout": go.Layout(title = "Matched vs. Unmatched")
-})
-plotly.offline.plot({
-    "data" : [go.Histogram(x = match_scores, name = "Match Score", xbins=dict(
-        start=-12,
-        end=0))],
-    "layout": go.Layout(title = "Match Scores")
-})
-
-data = read_quality_box_plot()
-
-layout = go.Layout(
-    title = "Read Quality Scores by Location (Percent Change of Accuracy)"
-)
-fig = go.Figure(data=data, layout=layout)
-plotly.offline.plot(fig)
